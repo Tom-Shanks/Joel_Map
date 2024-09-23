170d7cf1a5850b1ca32214e1373557bb04ace693
Tom <56661692+Tom-Shanks@users.noreply.github.com>
2024-09-23 08:41:05 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,2 @@
-function toggleModal(modalId) {
-    var modal = document.getElementById(modalId);
-    if (modal.classList.contains('show')) {
-        modal.classList.remove('show');
-    } else {
-        modal.classList.add('show');
-    }
+}
 }
